fa0a70ce586fe2f75266a703e7d7616ca647e40e
vignesh <palanivig12@gmail.com>
2024-02-08 12:14:11 +0530

    first commit

---
+++ gmb.py
@@ -0,0 +1,91 @@
+import pickle
+import os
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+import datetime
+
+
+def Create_Service(client_secret_file, api_name, api_version, *scopes):
+    """
+    Creates a service object for the specified API using the provided client secret file and scopes.
+
+    Args:
+        client_secret_file (str): The path to the client secret file.
+        api_name (str): The name of the API.
+        api_version (str): The version of the API.
+        *scopes (str): Variable number of scopes required for the API.
+
+    Returns:
+        service: The service object for the specified API.
+
+    Raises:
+        Exception: If unable to connect to the API.
+
+    """
+    print(client_secret_file, api_name, api_version, scopes, sep='-')
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
+    print(SCOPES)
+
+    cred = None
+
+    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
+    # print(pickle_file)
+
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as token:
+            cred = pickle.load(token)
+
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            cred = flow.run_local_server()
+
+        with open(pickle_file, 'wb') as token:
+            pickle.dump(cred, token)
+
+    try:
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
+        print(API_SERVICE_NAME, 'service created successfully')
+        return service
+    except Exception as e:
+        print('Unable to connect.')
+        print(e)
+        return None
+
+def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
+    dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
+    return dt
+
+CLIENT_SECRET_FILE = "client_secret.json" 
+API_NAME = 'mybusinessbusinessinformation'
+API_VERSION = 'v1'
+SCOPES = ['https://www.googleapis.com/auth/business.manage']
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+print(dir(service))
+new_business = service.accounts().locations().create(body={
+    "languageCode": "en-US",
+    "storeCode": "storeCode123",
+    "title": "storeName123",
+    "websiteUri": "https://www.google.com",
+    "categories":{
+        "primaryCategory": {
+            "name": "Shoe store123",
+            "displayName": "Shoe store",
+            }
+    },
+    "storefrontAddress": {
+        "revision": 0,
+        "regionCode": "US"
+  }
+}, parent="accounts/107696407368077692646")
+
+list_business = service.accounts().locations().list(parent="accounts/107696407368077692646")
+list_business.execute()
+
---
+++ jsonpayload.py
@@ -0,0 +1,244 @@
+# {
+#   "name": string,
+#   "languageCode": string,
+#   "storeCode": string,
+#   "title": string,
+#   "phoneNumbers": {
+#       "primaryPhone": string,
+#       "additionalPhones": [
+#           string
+#       ]
+#   },
+#   "categories": {
+#     "primaryCategory": {
+#       "name": string,
+#       "displayName": string,
+#       "serviceTypes": [
+#       {
+#           "serviceTypeId": string,
+#           "displayName": string
+#       }
+#       ],
+#       "moreHoursTypes": [
+#       {
+#           "hoursTypeId": string,
+#           "displayName": string,
+#           "localizedDisplayName": string
+#       }
+#       ]
+#       },
+#       "additionalCategories": [
+#       {
+#       "name": string,
+#       "displayName": string,
+#       "serviceTypes": [
+#       {
+#       "serviceTypeId": string,
+#       "displayName": string
+#       }
+#       ],
+#       "moreHoursTypes": [
+#       {
+#           "hoursTypeId": string,
+#           "displayName": string,
+#           "localizedDisplayName": string
+#       }
+#       ]
+#       }
+#       ]
+#   },
+#   "storefrontAddress": {
+#       "revision": integer,
+#       "regionCode": string,
+#       "languageCode": string,
+#       "postalCode": string,
+#       "sortingCode": string,
+#       "administrativeArea": string,
+#       "locality": string,
+#       "sublocality": string,
+#       "addressLines": [
+#               string
+#       ],
+#       "recipients": [
+#           string
+#       ],
+#       "organization": string
+#   },
+#   "websiteUri": string,
+#   "regularHours": {
+#     "periods": [
+#     {
+#       "openDay": enum (DayOfWeek),
+#       "openTime": {
+#             "hours": integer,
+#             "minutes": integer,
+#             "seconds": integer,
+#             "nanos": integer
+#             },
+#       "closeDay": enum (DayOfWeek), 
+#       "closeTime": {
+#             "hours": integer,
+#             "minutes": integer,
+#             "seconds": integer,
+#             "nanos": integer
+#             }
+#     }
+#     ]
+#   },
+#   "specialHours": {
+#     "specialHourPeriods": [
+#       {
+#       "startDate": 
+#       {
+#             "year": integer,
+#             "month": integer,
+#             "day": integer
+#       },
+#       "openTime": 
+#       {
+#             "hours": integer,
+#             "minutes": integer,
+#             "seconds": integer,
+#             "nanos": integer
+#       },
+#       "endDate": 
+#       {
+#             "year": integer,
+#             "month": integer,
+#             "day": integer
+#       },
+#       "closeTime": 
+#       {
+#             "hours": integer,
+#             "minutes": integer,
+#             "seconds": integer,
+#             "nanos": integer
+#       },
+#       "closed": boolean
+#   }
+#   ]
+#   },
+#   "serviceArea": {
+#     "businessType": enum (BusinessType),
+#     "places": 
+#     {
+#           "placeInfos": [
+#           {
+#               "placeName": string,
+#               "placeId": string
+#           }
+#           ]
+#     },
+#     "regionCode": string
+#   },
+#   "labels": [
+#       string
+#   ],
+#   "adWordsLocationExtensions": {
+#     "adPhone": string
+#   },
+#   "latlng": {
+#     "latitude": number,
+#     "longitude": number
+#   },
+#   "openInfo": {
+#     "status": enum (OpenForBusiness),
+#     "canReopen": boolean,
+#     "openingDate": 
+#     {
+#           "year": integer,
+#           "month": integer,
+#           "day": integer
+#     },
+#   "metadata": 
+#     {
+#           "hasGoogleUpdated": boolean,
+#           "hasPendingEdits": boolean,
+#           "canDelete": boolean,
+#           "canOperateLocalPost": boolean,
+#           "canModifyServiceList": boolean,
+#          "canHaveFoodMenus": boolean,
+#           "canOperateHealthData": boolean,
+#           "canOperateLodgingData": boolean,
+#           "placeId": string,
+#           "duplicateLocation": string,
+#           "mapsUri": string,
+#           "newReviewUri": string,
+#           "canHaveBusinessCalls": boolean,
+#           "hasVoiceOfMerchant": boolean
+#     },
+#   "profile": {
+#     "description": string
+#   },
+#   "relationshipData": {
+#     "parentLocation": 
+#     {
+#           "placeId": string,
+#           "relationType": enum (RelationType)
+#     },
+#     "childrenLocations": [
+#     {
+#           "placeId": string,
+#           "relationType": enum (RelationType)
+#     }
+#     ],
+#     "parentChain": string
+#   },
+#   "moreHours": [
+#     {
+#       "hoursTypeId": string,
+#       "periods": [
+#       {
+#       "openDay": enum (DayOfWeek),
+#       "openTime": {
+#             "hours": integer,
+#             "minutes": integer,
+#             "seconds": integer,
+#             "nanos": integer
+#             },
+#       "closeDay": enum (DayOfWeek), 
+#       "closeTime": {
+#             "hours": integer,
+#             "minutes": integer,
+#             "seconds": integer,
+#             "nanos": integer
+#             }
+#     
+#       }
+#       ]
+#     }
+#   ],
+#   "serviceItems": [
+#     {
+#       "price": 
+#       {
+#             "currencyCode": string,
+#             "units": string,
+#             "nanos": integer
+#       },
+# // Union field service_item_info can be only one of the following:
+#       "structuredServiceItem": 
+#       {
+#             "serviceTypeId": string,
+#             "description": string
+#       },
+#       "freeFormServiceItem": 
+#       {
+#             "category": string,
+#       "label": 
+#       {
+#             "displayName": string,
+#             "description": string,
+#             "languageCode": string
+#       }
+#       }
+# // End of list of possible types for union field service_item_info.
+#     }
+#   ]
+# }
+
+
+class JSONPayload:
+    def get_json_payload():
+        json_payload = input("Enter the JSON payload: ")
+        return json_payload
